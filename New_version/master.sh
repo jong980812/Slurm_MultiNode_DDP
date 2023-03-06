668f2c892a77e4262316aae973eeff69b3f5b4d3
Jongseo Lee <jong980812@gamil.com>
2023-03-07 02:34:56 +0900

    bug fix

--- New_version/master.sh
+++ New_version/master.sh
@@ -18,9 +18,9 @@ declare -a orders;
 # Orders list needs to be keep order sequene
 NODE_LIST["moana-y1"]=;         orders+=( "moana-y1" )
 NODE_LIST["moana-y2"]=;         orders+=( "moana-y2" )
-NODE_LIST["moana-y3"]=;        orders+=( "moana-y3" )
+NODE_LIST["moana-y3"]=;         orders+=( "moana-y3" )
 NODE_LIST["moana-y4"]=;         orders+=( "moana-y4" )
-NODE_LIST["moana-y5"]=1;         orders+=( "moana-y5" )
+NODE_LIST["moana-y5"]=1;        orders+=( "moana-y5" )
 NODE_LIST["moana-y6"]=1;        orders+=( "moana-y6" )
 NODE_LIST["moana-y7"]=2;        orders+=( "moana-y7" )
 #############Get MASTERNODE#########
@@ -79,7 +79,6 @@ echo -e "\033[2m\033[1m========================================================\
 
 
                                                                 
-####################################################################################################################################
 ####################################################################################################################################
 
 
